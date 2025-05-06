e916ed408f03c38d293669d6da1b01e55ad988c9
itspranavdubey <stsnetking@gmail.com>
2025-05-06 20:06:54 +0530

    Update sonar-project.js
    update sonar qube url and token - sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://18.171.209.223:9980/',
+  serverUrl: 'http://13.232.81.89:9980/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_95f458af2e63dfdddb65b4f3e2ccfb04b3455ea9',
+	    'sonar.login': 'squ_3506ae490a2fdacb98bb8355b7b2465f9d14c346',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
